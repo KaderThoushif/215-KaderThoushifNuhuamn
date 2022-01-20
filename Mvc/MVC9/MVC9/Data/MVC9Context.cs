a3ed8e17ea93d37ddb4659d49b9c366009ad590b
Thoushif <kaderthousif@gmail.com>
2022-01-20 17:26:07 +0530

    Mvc Projects

---
+++ Mvc/MVC9/MVC9/Data/MVC9Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC9.Models;
+
+namespace MVC9.Data
+{
+    public class MVC9Context : DbContext
+    {
+        public MVC9Context (DbContextOptions<MVC9Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC9.Models.Student> Student { get; set; }
+    }
+}
