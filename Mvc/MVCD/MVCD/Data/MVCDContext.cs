a1d443c870ed10ccbecf402b7956e8241bc3a356
Thoushif <kaderthousif@gmail.com>
2022-01-20 17:12:35 +0530

    Mvc Projects

---
+++ Mvc/MVCD/MVCD/Data/MVCDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCD.Models;
+
+namespace MVCD.Data
+{
+    public class MVCDContext : DbContext
+    {
+        public MVCDContext (DbContextOptions<MVCDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCD.Models.Department> Department { get; set; }
+    }
+}
---
+++ Mvc/MVCD/MVCD/Models/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCD.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+
+        public string DeptName { get; set; }
+
+        public string Location { get; set; }
+
+        public int NumberofStaff { get; set; }
+
+
+    }
+}
