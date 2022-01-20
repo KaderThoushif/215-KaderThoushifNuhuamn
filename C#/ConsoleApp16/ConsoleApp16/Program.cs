86fb3a96082081defbf8fa846506ae523f92deb4
Thoushif <96901997+KaderThoushif@users.noreply.github.com>
2022-01-20 16:18:22 +0530

    Add files via upload

---
+++ C#/ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack mystack = new Stack();
+            mystack.Push("Hello");
+            mystack.Push("null");
+            mystack.Push(1);
+            mystack.Push(2);
+            mystack.Push(3);
+            mystack.Push(4);
+            mystack.Push(5);
+
+            foreach(var item in mystack)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        
+    }
+}
